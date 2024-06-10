1b56c31494eeeea3c8ee2dd68519e59ec4fdcbbe
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-10 17:11:40 +0200

    feat: add slider function when clicking right arrow icon up to the last image

--- js/main.js
+++ js/main.js
@@ -30,3 +30,35 @@ for (let i = 0; i < carouselArray.length; i++){
 
 
 }
+
+//inserire tutti gli elementi di markup in base alla classe ".item"
+
+const items = document.getElementsByClassName("item");
+
+//provare a inserire la classe ".active" per ogni singolo elemento
+let activeItems = 0;
+items[activeItems].classList.add("active");
+
+//selezionare l'id del bottone che fa scorrere lo slider
+const nextButton = document.querySelector("#right");
+
+//creare un evento che permetta di avanzare le immagini al click
+nextButton.addEventListener('click',
+    function(){
+
+        //fare in modo di fermarsi all'ultima immagine
+        if(activeItems < carouselArray.length - 1){
+
+            // Rimuovere la classe active dell'immagine precedente 
+            items[activeItems].classList.remove("active");
+
+            // Incrementare il valore di activeItems e gestire i limiti
+            activeItems = (activeItems + 1);
+
+            // Associare la classe active agli items
+            items[activeItems].classList.add("active");
+
+        }
+        
+    }
+);
