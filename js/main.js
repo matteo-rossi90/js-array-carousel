856e8a43b8b8dea1e45bce1972c0cc4440e72c1b
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-10 16:20:20 +0200

    feat: add js code to insert image list and markup element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 /* creare un carosello delle immagini nel documenti HTML */
+
+//inserire le immagini del carosello da html a js
+
+//creare un array che contiene le immagini
+const carouselArray = [
+    'img/01.webp', 
+    'img/02.webp', 
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp' 
+];
+
+//selezionare il container che raccoglierà le immagini
+const itemContainer = document.querySelector("#item-container");
+
+//estrapolare i singoli dati
+
+for (let i = 0; i < carouselArray.length; i++){
+    let itemImg = carouselArray[i];
+
+    //creare per ogni iterazione l'elemento di markup che conterrà l'immagine
+    let itemContent = `<div class="item">
+                            <img src="${itemImg}">
+                        </div>`;
+    
+    //fare in modo che il contenuto dei div di markup e le rispettive immagini
+    //siano inserite nel contenitore selezionato
+    itemContainer.innerHTML += itemContent;
+
+
+}
